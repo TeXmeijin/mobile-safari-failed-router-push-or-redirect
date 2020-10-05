34b8aa9749d72f9592b93cfb5a8ae8e443b355b8
TeXmeijin <i8858garden@gmail.com>
2020-10-05 16:33:50 +0900

    api通信

--- middleware/redirect.ts
+++ middleware/redirect.ts
@@ -1,10 +1,25 @@
 import { Middleware } from "@nuxt/types";
 
-
-const redirect: Middleware = ({redirect}) => {
-    redirect({
-        name: 'index'
+const fetchData = () => {
+    return new Promise((resolve) => {
+        // may cause CORS error
+        fetch('https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060')
+            .then(
+                (response) => {
+                    resolve(response.json())
+                }
+            )
+            .catch(() => resolve())
     })
+}
+
+const redirect: Middleware = async ({redirect}) => {
+    if (process.client) {
+        await fetchData()
+        redirect({
+            name: 'index'
+        })
+    }
 }
 
 export default redirect
